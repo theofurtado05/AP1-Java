32cbe58c45447bbfc13265f9190ab69fc971d193
Theo Furtado <theo.furtado@linerasistemas.com.br>
2023-04-25 18:23:01 -0300

    isIngressoDisponivel almost OK

--- src/Partida.java
+++ src/Partida.java
@@ -39,7 +39,20 @@ public class Partida {
 
     //verifica se há ingressos disponíveis para o tipo e quantidade especificados; isIngressoDisponivel(TipoIngresso tipo, int quantidade)
     public boolean isIngressoDisponivel(TipoIngresso tipo, int quantidade){
-        return true;
+        if(tipo == TipoIngresso.MEIA){
+            //verifica meia
+            if(ingressosMeia > quantidade){
+                System.out.println(ingressosMeia + " ingressos meia estao disponiveis no momento.");
+                return true;
+                } 
+            } else if(tipo == TipoIngresso.INTEIRA){
+            //verifica inteira
+            if(ingressosInteira > quantidade){
+                System.out.println(ingressosInteira + " ingressos meia estao disponiveis no momento.");
+                return true;
+            } 
+        }
+        return false;
     }
 
     //Método venderIngresso(TipoIngresso tipo): realiza a venda de ingressos do tipo e quantidade especificados e retorna o valor total da venda;
